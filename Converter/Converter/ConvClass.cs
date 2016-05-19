9b058d644affb5cb0f2c00700aaf150f9f02c2a1
Aleksandra <Aleksandra@XP.PL>
2016-05-19 16:10:34 -0700

    'c2'

---
+++ Converter/Converter/ConvClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class ConvClass
+    {
+        private float rate = 0;
+        public void setRate(float r)
+        {
+            if (r > 0)
+            {
+                rate = r;
+            }
+
+        }
+        public float convertFrom(float a)
+        {
+            return a * rate;
+        }
+        public float convertTo(float a)
+        {
+            return a / rate;
+        }
+       
+
+    }
+}
---
+++ Converter/Converter/Test/ConversionTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Test
+{
+    [TestFixture]
+    public class ConversionTest
+    {
+        [Test]
+        public void TestConversionFrom()
+        {
+            float amount = 12.5f;
+            ConvClass c = new ConvClass();
+            c.setRate(2.3f);
+            float result= c.convertFrom(amount);
+            if (result != 28.75f)
+            {
+                Assert.Fail("Wrong conversion result");
+            }
+            Assert.Pass("Correct conversion");
+        }
+
+        [Test]
+        public void TestConversionTo()
+        {
+            float amount = 28.75f;
+            ConvClass c = new ConvClass();
+            c.setRate(2.3f);
+            float result = c.convertTo(amount);
+            if (result != 12.5f)
+            {
+                Assert.Fail("Wrong conversion result");
+            }
+            Assert.Pass("Correct conversion");
+        }
+    }
+}
