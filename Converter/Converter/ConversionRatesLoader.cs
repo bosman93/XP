9a29ce3f6d5caef2cdae07b80ee8c2005d04c40a
Mariusz <Mariusz@xp.pl>
2016-05-19 16:31:07 -0700

    add loader stub

---
+++ Converter/Converter/ConversionRatesLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class ConversionRatesLoader
+    {
+        internal object load()
+        {
+            var conversionRates = new List<ConversionRate>();
+        }
+    }
+}
---
+++ Converter/Converter/Test/ConversionRatesLoaderTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Test
+{
+    [TestFixture]
+    public class ConversionRatesLoaderTest
+    {
+        [Test]
+        public void TestValidFormat()
+        {
+            ConversionRatesLoader loader = new ConversionRatesLoader();
+            Assert.Throws<FormatException>(() => loader.load());
+        }
+    }
+}
