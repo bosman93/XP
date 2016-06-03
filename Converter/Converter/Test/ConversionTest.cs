8704c2a0064261b1991b7dc2a285c61e19db8c13
Lukasz <Lukasz@xp.pl>
2016-06-02 17:36:43 -0700

    refactoring

--- Converter/Converter/Test/ConversionTest.cs
+++ Converter/Converter/Test/ConversionTest.cs
@@ -5,34 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Converter.Test
-{
+namespace Converter.Test {
     [TestFixture]
-    public class ConversionTest
-    {
+    public class ConversionTest {
+
         [Test]
-        public void TestConversionFrom()
-        {
+        public void TestConversionFrom() {
             float amount = 12.5f;
             ConvClass c = new ConvClass();
             c.setRate(2.3f);
-            float result= c.convertFrom(amount);
-            if (result != 28.75f)
-            {
+            float result = c.convertFrom(amount);
+            if (result != 28.75f) {
                 Assert.Fail("Wrong conversion result");
             }
             Assert.Pass("Correct conversion");
         }
 
         [Test]
-        public void TestConversionTo()
-        {
+        public void TestConversionTo() {
             float amount = 28.75f;
             ConvClass c = new ConvClass();
             c.setRate(2.3f);
             float result = c.convertTo(amount);
-            if (result != 12.5f)
-            {
+            if (result != 12.5f) {
                 Assert.Fail("Wrong conversion result");
             }
             Assert.Pass("Correct conversion");
